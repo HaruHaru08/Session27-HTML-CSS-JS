99a0ee196b71b3b5c8440e78709ef8230def970c
HaruHaru <cuong33442@gmail.com>
2025-03-14 15:37:50 +0700

    first commit

--- b5.js
+++ b5.js
@@ -1,6 +1,6 @@
 function containsArr(arr,n){
     let newArr=[];
-    let num = Math.ceil(arr.length / n);//Lấy trên chat cái này thì mới giống đề được thầy chỉ thiếu rồi
+    let num = Math.ceil(arr.length / n);
 for(let i=0;i<arr.length;i+=n){
     let  putArr=arr.slice(i,i+num);
     newArr.push(putArr);
